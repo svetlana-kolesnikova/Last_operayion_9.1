508320f8f16cae288b802987faf573015e6107d8
Svetlana Kolesnikova <sv.ps1286@gmail.com>
2025-04-22 07:34:37 +0300

    fixes filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -1,15 +1,10 @@
 def filter_by_state(database: list, state: str = "EXECUTED") -> list:
-    """Функция возвращает новй список словарей с указанным значением state"""
-    new_database = []
-    for i in database:
-        for key in i.keys():
-            if i[key] == state:
-                new_database.append(i)
-
+    """Функция возвращает новый список словарей с данными банковских карт со значением state (по умолчанию)"""
+    new_database = [item for item in database if item.get("state") == state]
     return new_database
 
 
 def sort_by_date(date: list, ascending: bool = True) -> list:
-    """Функция сортирует данные по дате и по убыванию (поумолчанию)"""
+    """Функция сортирует данные по дате и по убыванию (по умолчанию)"""
     data_sorted = sorted(date, key=lambda x: x.get("date", 0), reverse=ascending)
     return data_sorted
