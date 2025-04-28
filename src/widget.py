94c71082e15bc060b21f8032b833946f5ae337b3
Svetlana Kolesnikova <sv.ps1286@gmail.com>
2025-04-28 07:19:18 +0300

    fixes fnc mask_account_card in widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,16 +1,26 @@
 def mask_account_card(account_card: str) -> str:
     """Функция обрабатывает информацию как о картах, так и о счетах"""
     split_account_card = account_card.split(" ")
-    if "Счет" in account_card:
+    if ("Счет" in account_card and len(account_card) != 25
+            or "Счет" in account_card and not account_card[5:].isdigit()):
+        raise ValueError("Invalid type of number")
+
+    elif account_card[-17] == " " and not account_card[-16:].isdigit():
+        raise ValueError("Invalid type of number")
+
+    elif "Счет" in account_card and len(account_card) == 25:
         mask_account_number = split_account_card[-1].replace(split_account_card[-1][0:16], "**")
         split_account_card[-1] = mask_account_number
         account_number_masked = " ".join(split_account_card)
 
         return account_number_masked
 
+    elif account_card[-17] != " " and not account_card[-16:].isdigit():
+        raise ValueError("Invalid type of number")
+
     else:
         mask_number_card = (
-            f"{split_account_card[-1][:4]} {split_account_card[-1][4:6]}** ****{split_account_card[-1][12:]}"
+            f"{split_account_card[-1][:4]} {split_account_card[-1][4:6]}** **** {split_account_card[-1][12:]}"
         )
     split_account_card[-1] = mask_number_card
     number_card_masked = " ".join(split_account_card)
