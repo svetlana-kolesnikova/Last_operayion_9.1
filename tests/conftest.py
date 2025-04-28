38552172b0b1994a6ee96cd6eb5c56f94cf98a08
Svetlana Kolesnikova <sv.ps1286@gmail.com>
2025-04-28 23:33:48 +0300

    add fixtures for test_processing.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,3 +9,44 @@ def digit_number_wrong():
 @pytest.fixture
 def digit_alpha_number_wrong():
     return "card1234_alpha"
+
+
+@pytest.fixture
+def sample_list_of_data_base():
+    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+            {'id': 615064591, 'date': '2018-10-14T08:21:33.419441'}]
+
+
+@pytest.fixture
+def sample_list_of_data_base_executed_without_state():
+    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 594226727, 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+            {'id': 615064591, 'date': '2018-10-14T08:21:33.419441'}]
+
+
+@pytest.fixture
+def list_of_data_base_with_state_executed():
+    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+
+
+@pytest.fixture
+def list_of_data_base_with_state_canceled():
+    return [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+
+@pytest.fixture
+def state_executed():
+    return 'EXECUTED'
+
+
+@pytest.fixture
+def state_canceled():
+    return 'CANCELED'
