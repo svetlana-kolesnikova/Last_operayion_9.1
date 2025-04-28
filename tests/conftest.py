9a3348ed6bc0d35fdcb81636f5e28c4e6354c491
Svetlana Kolesnikova <sv.ps1286@gmail.com>
2025-04-28 23:56:35 +0300

    add tests for fnc sort_by_date and fixtures for test_processing.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,18 +16,17 @@ def sample_list_of_data_base():
     return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
             {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
             {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-            {'id': 615064591, 'date': '2018-10-14T08:21:33.419441'}]
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
 
 @pytest.fixture
 def sample_list_of_data_base_executed_without_state():
     return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-            {'id': 594226727, 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 594226727, 'date': '2018-09-13T21:27:25.241689'},
             {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
             {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
             {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-            {'id': 615064591, 'date': '2018-10-14T08:21:33.419441'}]
+            {'id': 615064591, 'date': '2018-11-14T08:21:33.419441'}]
 
 
 @pytest.fixture
@@ -50,3 +49,20 @@ def state_executed():
 @pytest.fixture
 def state_canceled():
     return 'CANCELED'
+
+@pytest.fixture
+def sample_list_sort_by_date():
+    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+
+
+@pytest.fixture
+def sample_list_of_data_base_sorted_by_date():
+    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 615064591, 'date': '2018-11-14T08:21:33.419441'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+            {'id': 594226727, 'date': '2018-09-13T21:27:25.241689'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,4 +1,4 @@
-from src.processing import filter_by_state
+from src.processing import filter_by_state, sort_by_date
 
 
 def test_filter_by_state_with_executed(sample_list_of_data_base: list, state_executed: str,
@@ -23,3 +23,8 @@ def test_filter_by_state_without_state_in_list(sample_list_of_data_base_executed
                                                list_of_data_base_with_state_executed: list) -> None:
     """Тестирование функции без указания параметра "state" """
     assert filter_by_state(sample_list_of_data_base_executed_without_state) == list_of_data_base_with_state_executed
+
+
+def test_sort_by_date(sample_list_of_data_base_executed_without_state: list,
+                      sample_list_of_data_base_sorted_by_date: list) -> None:
+    assert sort_by_date(sample_list_of_data_base_executed_without_state) == sample_list_of_data_base_sorted_by_date
