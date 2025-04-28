530c0f4489ec9fc8efbcb5d23b22374d26b1498f
Svetlana Kolesnikova <sv.ps1286@gmail.com>
2025-04-28 22:09:22 +0300

    add tests for fnc get_date to test_widget.py

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.widget import mask_account_card
+from src.widget import mask_account_card, get_date
 
 
 @pytest.mark.parametrize(
@@ -33,7 +33,19 @@ def test_mask_account_card_right(example_count_number: str, example_masked_count
     ],
 )
 def test_mask_account_card_error_type(example_count_number: str, error_message: str) -> None:
-    """тестирование вызова ошибки при передаче неверного количества символов"""
+    """Тестирование вызова ошибки при передаче неверного количества символов"""
     with pytest.raises(ValueError) as e_info:
         mask_account_card(example_count_number)
     assert str(e_info.value) == error_message
+
+
+def test_get_date_right() -> None:
+    """ Тест с правильно введённой датой"""
+    assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
+
+
+def test_get_date_wrong_data_format() -> None:
+    """Тестирование вызова ошибки при передаче неверного количества символов"""
+    with pytest.raises(ValueError) as e_info:
+        get_date("2024-0K-11T02:26:18.671407")
+    assert str(e_info.value) == "Invalid type of date"
