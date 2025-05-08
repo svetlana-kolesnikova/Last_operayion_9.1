48240a90b4087ab1a374b90c3b838fd701ff24cb
Svetlana Kolesnikova <sv.ps1286@gmail.com>
2025-05-08 17:23:57 +0300

    fixes conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -82,3 +82,74 @@ def sample_list_of_data_base_sorted_by_date_reverted():
 @pytest.fixture
 def ascending_false():
     return False
+
+
+@pytest.fixture
+def sample_transactions_usd():
+    return [
+        {
+
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD","code": "USD"}},
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        },
+        {
+            "id": 142264265,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        }
+    ]
+
+
+@pytest.fixture
+def sample_currency_usd():
+    return "USD"
+
+
+@pytest.fixture
+def sample_transactions():
+    return [{
+              "id": 939719570,
+              "state": "EXECUTED",
+              "date": "2018-06-30T02:08:58.425572",
+              "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+              "description": "Перевод организации",
+              "from": "Счет 75106830613657916952",
+              "to": "Счет 11776614605963066702"
+       },
+       {
+              "id": 142264265,
+              "state": "EXECUTED",
+              "date": "2019-04-04T23:20:05.206878",
+              "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "RUB"}},
+              "description": "Перевод со счета на счет",
+              "from": "Счет 19708645243227258542",
+              "to": "Счет 75651667383060284188"
+       },
+       {
+              "id": 142264268,
+              "state": "EXECUTED",
+              "date": "2019-04-04T23:20:05.206878",
+              "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+              "description": "Перевод со счета на счет",
+              "from": "Счет 19708645243227258542",
+              "to": "Счет 75651667383060284188"
+       },
+       {
+              "id": 142264246,
+              "state": "EXECUTED",
+              "date": "2019-04-04T23:20:05.206878",
+              "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "RUB"}},
+              "description": "Перевод со счета на счет",
+              "from": "Счет 19708645243227258542",
+              "to": "Счет 75651667383060284188"
+       },
+    ]
