660aec62bdce704ddb711b9061b89510924fa80d
Svetlana Kolesnikova <sv.ps1286@gmail.com>
2025-05-11 18:40:17 +0300

    add decorators.py with decorator log

---
+++ src/decorators.py
@@ -0,0 +1,36 @@
+from typing import Any, Callable, Union
+
+
+def log(filename=None):
+    """Декоратор для логирования функций и вывода результатов в консоль или файл"""
+    def wrapper(fnc: Callable) -> Any:
+        def inner(*args, **kwargs) -> Any:
+            try:
+                result = fnc(*args, **kwargs)
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(f"{fnc.__name__} ok\n")
+                else:
+                    print(f"{fnc.__name__} ok\n")
+                return result
+
+            except Exception as e:
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(f"{fnc.__name__} error: {e}. Inputs: {args}, {kwargs}\n")
+                else:
+                    print(f"{fnc.__name__} error: {e}. Inputs: {args}, {kwargs}\n")
+                raise Exception("Деление на ноль невозможно")
+
+        return inner
+    return wrapper
+
+
+if __name__ == '__main__':
+
+    @log(filename="mylog.txt")  # filename="mylog.txt" filename=None
+    def my_function(x: Union[int, float], y: Union[int, float]) -> Any:
+        """Функция для проверки работы декоратора"""
+        return x / y
+
+    print(my_function(6, 3))
