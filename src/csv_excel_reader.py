a6731379a118c82a677b2497edf5dc507d06eb1b
Svetlana Kolesnikova <sv.ps1286@gmail.com>
2025-06-08 11:56:22 +0300

    fixing csv_excel_reader.py, config.py, utils.py, external_api.py

--- src/csv_excel_reader.py
+++ src/csv_excel_reader.py
@@ -1,19 +1,14 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, List, cast
 
 import pandas as pd
-
-from config import PATH
-
-path_to_csv = PATH / "data" / "transactions.csv"
-path_to_excel = PATH / "data" / "transactions_excel.xlsx"
 
 
 def reader_csv(path: Any) -> List[Dict[Any, Any]]:
     """Функция для чтения файла csv. Возвращает список словарей с транзакциями"""
     try:
-        reader = pd.read_csv(path, delimiter = ";")
+        reader = pd.read_csv(path, delimiter=";")
         financial_operations_csv = reader.to_dict(orient="records")
-        return financial_operations_csv
+        return cast(List[Dict[Any, Any]], financial_operations_csv)
     except (FileNotFoundError, Exception):
         return []
 
@@ -23,6 +18,6 @@ def reader_excel(path: Any) -> List[Dict[Any, Any]]:
     try:
         reader = pd.read_excel(path)
         financial_operations_excel = reader.to_dict(orient="records")
-        return financial_operations_excel
+        return cast(List[Dict[Any, Any]], financial_operations_excel)
     except (FileNotFoundError, Exception):
         return []
